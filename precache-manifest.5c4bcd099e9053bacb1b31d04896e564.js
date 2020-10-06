59799d01f2e3112d5daf973c0c15305ae1df6a45
Doverstav <pontus.doverstav@kits.se>
2020-10-06 16:29:26 +0200

    Updates

---
+++ precache-manifest.5c4bcd099e9053bacb1b31d04896e564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6f2b49633796aa6a9f20cc51f7781d",
+    "url": "/kitscon202/index.html"
+  },
+  {
+    "revision": "6b4e81d2485174606f40",
+    "url": "/kitscon202/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2ac71f7f41044b046bcc",
+    "url": "/kitscon202/static/js/2.f904bc69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kitscon202/static/js/2.f904bc69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4e81d2485174606f40",
+    "url": "/kitscon202/static/js/main.a936ddc8.chunk.js"
+  },
+  {
+    "revision": "111ef10b6c97fa11a82b",
+    "url": "/kitscon202/static/js/runtime-main.7d080dcd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/kitscon202/static/media/logo.ee7cd8ed.svg"
+  }
+]);
