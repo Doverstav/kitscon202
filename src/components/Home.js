a23854de49503b86dbb7e37e9c788d5e2b223b12
Doverstav <pontus.doverstav@kits.se>
2020-10-16 18:41:22 +0200

    Do some fonting

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Home(props) {
   return (
     <div>
+      <h1>Here's a nice lil headline!</h1>
       <p>
         Welcome home! This page should probably work as an expaliner for what
         this site is trying to do. I could probably bake the "Exclusive
