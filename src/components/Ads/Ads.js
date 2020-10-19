b05c9ad4885d7d929ea5813e1a88a8c5b3708ed0
Doverstav <pontus.doverstav@kits.se>
2020-10-19 15:39:59 +0200

    Write short text for Ads screen

--- src/components/Ads/Ads.js
+++ src/components/Ads/Ads.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Home(props) {
-  return <div>Here we demo removable ads!</div>;
+  return (
+    <div>
+      <h1>Removable ads</h1>
+      <p>
+        Allow the user to choose! Either they can pay you directly using Web
+        Monetization, or you can serve them some ads if they do not use it.
+      </p>
+      <p>Toogle Web monetization on and off to see the difference.</p>
+    </div>
+  );
 }
