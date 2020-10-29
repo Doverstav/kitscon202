2cebc961babdfeefe8a35286b40ca6f58bbb6b00
Doverstav <pontus.doverstav@kits.se>
2020-10-29 17:20:44 +0100

    First draft of home screen text

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,15 +55,41 @@ export default function Home(props) {
 
   return (
     <div>
-      <h1>Here's a nice lil headline!</h1>
+      <h1>Web Monetization Demo</h1>
       <p>
-        Welcome home! This page should probably work as an expaliner for what
-        this site is trying to do. I could probably bake the "Exclusive
-        Content"-demo into this page as well.
+        <Link href="https://webmonetization.org/" text="Web Monetization" /> is
+        a browser API that allows the creation of a payment stream from the user
+        agent to a website. This website contains simple examples of how Web
+        Monetization can be used, and tries to show that it is rather simple to
+        create a Web Monetized website.
       </p>
       <p>
-        Look at this text that is ended with a nice looking link:{" "}
-        <Link href="/" text="Click me!" />
+        As Web Monetization is a proposed standard, it is not part of any
+        browser today. Instead, users must have a Web Monetization provider, of
+        which there today exists only one,{" "}
+        <Link href="https://coil.com/" text="Coil" />. So, in order to make this
+        website usable by anyone, it allows the user to inject a fake Web
+        Monetization provider. This provider can also easily be removed to
+        easily compare between monetized and non-monetized versions.
+      </p>
+      <p>
+        This startpage demonstrates the so called{" "}
+        <Link
+          href="https://coil.com/p/coil/The-100-20-Rule-for-Premium-Content/3l1ALJ3M6"
+          text="100+20 rule"
+        />{" "}
+        for written content, where the free content is a complete article, but
+        monetized users can view some extra content.{" "}
+        {/*As an example, when reading
+        a recipe, the additional content may be variations on the recipe, or the
+        recipe to some side dish that goes great with the main recipe.*/}
+      </p>
+      <p>
+        The bonus content for this page can be seen at the bottom. Click the
+        "Start Web Monetization" to inject the monetization object and access
+        the exclusive content! Below that button you will also find whether the
+        current page has a Web Monetization tag, the current monetization state
+        and how much (fake) money has been sent during this session.
       </p>
       <Separator />
       <p>
@@ -74,12 +100,13 @@ export default function Home(props) {
           "Very nice! Please enjoy your exlusive content."}
       </p>
       {monetizationState === "started" ? (
-        <div>
-          <img src={BananaGif} alt="BananaGif" />
-          <p className="Home-text-small">
-            Gif taken from <Link href="http://gph.is/2o30x8S" text="here" />
-          </p>
-        </div>
+        <p>
+          So this is the exlusive content you were promised! It's not a lot, but
+          here it is. I can't give you much, but I can give you this advice.
+          Explore the other pages on this site via the navigation the your left
+          to see more examples! And visit the "About" page for more details on
+          how this all works.
+        </p>
       ) : null}
     </div>
   );
