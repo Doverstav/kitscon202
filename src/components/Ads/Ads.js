15720bd73ec6df229197feaf70c5713c27486990
Doverstav <pontus.doverstav@kits.se>
2020-11-11 11:14:23 +0100

    Remove side 'ads' so pages look more uniform and to decrease clutter

--- src/components/Ads/Ads.js
+++ src/components/Ads/Ads.js
@@ -9,7 +9,6 @@ import {
 } from "../WebMonetizationSpoofer/WebMonetizationSpoofer";
 
 import "./Ads.css";
-import Ad160x600 from "../../res/ad160x600.png";
 import Ad250x300 from "../../res/ad250x300.png";
 import Ad728x90 from "../../res/ad728x90.png";
 import Preamble from "../common/Preamble";
@@ -56,37 +55,29 @@ export default function Home(props) {
 
   return (
     <div className="Ads-container">
+      <h1>Double dipping in revenue streams</h1>
       {monetizationState !== WM_STATE_STARTED ? (
-        <img className="Ads-ad-side" src={Ad160x600} alt="" />
+        <img className="Ads-ad-banner" src={Ad728x90} alt="" />
       ) : null}
-      <div className="Ads-content">
-        <h1>Double dipping in revenue streams</h1>
-        {monetizationState !== WM_STATE_STARTED ? (
-          <img className="Ads-ad-banner" src={Ad728x90} alt="" />
-        ) : null}
-        <Preamble>
-          You don't have to put all your eggs in the Web Monetization-basket! It
-          is easy to combine a more traditional, ad-driven revenue stream with
-          Web Monetization. A monetized user won't have to their page bogged
-          down with advertisements, and a non-paying user will instead pay for
-          themselves by viewing your tastefully inserted ads.
-        </Preamble>
-        {monetizationState !== WM_STATE_STARTED ? (
-          <img className="Ads-ad-box" src={Ad250x300} alt="" />
-        ) : null}
-        <h2>Get the ad free experience</h2>
-        <p>
-          To remove the ads, turn on Web Monetization. After a second, when the
-          Web Monetization status has gone from pending to started, the ads will
-          disappear! To bring them back, simply stop the Web Monetization again,
-          and the ads will pop back in!
-        </p>
-        {monetizationState !== WM_STATE_STARTED ? (
-          <img className="Ads-ad-banner" src={Ad728x90} alt="" />
-        ) : null}
-      </div>
+      <Preamble>
+        You don't have to put all your eggs in the Web Monetization-basket! It
+        is easy to combine a more traditional, ad-driven revenue stream with Web
+        Monetization. A monetized user won't have to their page bogged down with
+        advertisements, and a non-paying user will instead pay for themselves by
+        viewing your tastefully inserted ads.
+      </Preamble>
       {monetizationState !== WM_STATE_STARTED ? (
-        <img className="Ads-ad-side" src={Ad160x600} alt="" />
+        <img className="Ads-ad-box" src={Ad250x300} alt="" />
+      ) : null}
+      <h2>Get the ad free experience</h2>
+      <p>
+        To remove the ads, turn on Web Monetization. After a second, when the
+        Web Monetization status has gone from pending to started, the ads will
+        disappear! To bring them back, simply stop the Web Monetization again,
+        and the ads will pop back in!
+      </p>
+      {monetizationState !== WM_STATE_STARTED ? (
+        <img className="Ads-ad-banner" src={Ad728x90} alt="" />
       ) : null}
     </div>
   );
